acd99751a404a83a513409df3e861aadfb7fa827
Akashwadbude <112239404+Akashwadbude@users.noreply.github.com>
2023-10-14 23:47:01 +0530

    Create main.py
    my python

---
+++ main.py
@@ -0,0 +1,14 @@
+# Get the user's name
+name = input("What is your name? ")
+
+# Greet the user
+print(f"Hello, {name}!")
+
+# Get the user's age
+age = int(input("How old are you? "))
+
+# Check if the user is of legal drinking age
+if age >= 21:
+    print("You are of legal drinking age. Enjoy responsibly!")
+else:
+    print("You are not of legal drinking age. Please refrain from drinking alcohol.")
